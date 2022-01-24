8ac6290ffc36d8f86965b01cc0b0f717667b3c72
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-01-24 11:50:09 -0700

    adds the start of the UI design

--- app/src/main/java/com/example/android_example/MainActivity.java
+++ app/src/main/java/com/example/android_example/MainActivity.java
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //relates to the activity_main.xml
     }
 
-    public void onBtnClick(View view){
-        TextView txt_first_name = findViewById(R.id.firstNameView); //R is the resources for the project
-        TextView txt_last_name = findViewById(R.id.lastNameView);
-        TextView txt_email = findViewById(R.id.emailView);
-        EditText edit_first_name = findViewById(R.id.editFirstName);
-        EditText edit_last_name = findViewById(R.id.editLastName);
-        EditText edit_email = findViewById(R.id.editEmail);
-        txt_first_name.setText(edit_first_name.getText().toString());
-        txt_last_name.setText(edit_last_name.getText().toString());
-        txt_email.setText(edit_email.getText().toString());
+    public void onHelloBtnClicked(View view){
+        TextView welcome_txt = findViewById(R.id.txtWelcome);
+        welcome_txt.setText("Hello again");
     }
+
 }
