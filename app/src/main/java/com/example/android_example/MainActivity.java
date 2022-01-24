557295ba86d7c748cf4b6990ac28cfcbaccbe858
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-01-24 11:14:17 -0700

    adds more user input including first and last name and email

--- app/src/main/java/com/example/android_example/MainActivity.java
+++ app/src/main/java/com/example/android_example/MainActivity.java
@@ -16,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClick(View view){
-        TextView txt_hello = findViewById(R.id.txtMessage); //R is the resources for the project
-        EditText editTextName = findViewById(R.id.editTxtName);
-        txt_hello.setText("Hello " + editTextName.getText().toString());
+        TextView txt_first_name = findViewById(R.id.firstNameView); //R is the resources for the project
+        TextView txt_last_name = findViewById(R.id.lastNameView);
+        TextView txt_email = findViewById(R.id.emailView);
+        EditText edit_first_name = findViewById(R.id.editFirstName);
+        EditText edit_last_name = findViewById(R.id.editLastName);
+        EditText edit_email = findViewById(R.id.editEmail);
+        txt_first_name.setText(edit_first_name.getText().toString());
+        txt_last_name.setText(edit_last_name.getText().toString());
+        txt_email.setText(edit_email.getText().toString());
     }
 }
