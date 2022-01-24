d1bb6013d5a7148fdb45f6b756c6b86e2e6a114e
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-01-24 10:50:16 -0700

    adds on click event for button

--- app/src/main/java/com/example/android_example/MainActivity.java
+++ app/src/main/java/com/example/android_example/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.android_example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //code runs on app start
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //relates to the activity_main.xml
+    }
+
+    public void onBtnClick(View view){
+        TextView txt_hello = findViewById(R.id.txtMessage); //R is the resources for the project
+        txt_hello.setText("Hello");
     }
 }
